ed20311c8bddd90b879ae77df1104d158c59e401
HaaVad <123933986+HaaVad@users.noreply.github.com>
2023-06-15 13:51:32 +0200

    Latest update

---
+++ sanity.config.ts
@@ -0,0 +1,23 @@
+import {defineConfig} from 'sanity';
+import {deskTool} from 'sanity/desk';
+import schemas from './sanity/schemas'
+
+const config = defineConfig({
+
+    projectId: "m7f72ydq",
+
+    dataset: "production",
+
+    title: "My Portfolio",
+
+    apiVersion: "2023-12-06",
+
+    basePath: "/admin",
+
+    plugins: [deskTool()],
+
+    schema: {types: schemas}
+
+})
+
+export default config;
---
+++ sanity/schemas/sanity-utils.ts
@@ -0,0 +1,24 @@
+import {createClient, groq} from "next-sanity";
+import { Project } from "../../types/Project";
+
+export async function getProjects(): Promise<Project[]> {
+    const client = createClient({
+        projectId: "m7f72ydq",
+
+        dataset: "production",
+    
+        apiVersion: "2023-12-06",
+    });
+
+    return client.fetch(
+        groq`*[_type = "project"]{
+            _id,
+            _createdAt,
+            name,
+            "slug": slug.current,
+            "image": image.asset=>url,
+            url,
+            content
+        }`
+    )
+}
